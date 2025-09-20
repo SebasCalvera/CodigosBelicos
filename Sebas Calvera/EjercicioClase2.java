355da81c1b26133c2c9bf3dbca7495e8a55bad10
SebasCalvera <112514206+SebasCalvera@users.noreply.github.com>
2025-09-19 21:29:38 -0500

    Add files via upload

---
+++ Sebas Calvera/EjercicioClase2.java
@@ -0,0 +1,19 @@
+public class EjercicioClase2 {
+    public static void main(String[] args) {
+        
+   ListaEnlazada miLista = new ListaEnlazada();
+
+   //Agregando elementos a la lista
+   miLista.agregar(1);
+   miLista.agregar(2);
+   miLista.agregar(3);
+   miLista.agregar(4);
+
+   System.out.println(" Lista despues de agregar elemntos: ");
+   milista.imprimir(); // Salida: 1 -> 2 -> 3 -> 4 -> null
+
+
+
+
+    }
+}
