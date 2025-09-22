00e5c38825d2b0ff7bd56577b8fb56991881e84b
SebasCalvera <112514206+SebasCalvera@users.noreply.github.com>
2025-09-22 17:36:44 -0500

    Add files via upload

--- Sebas Calvera/EjercicioClase2.java
+++ Sebas Calvera/EjercicioClase2.java
@@ -9,10 +9,13 @@ public class EjercicioClase2 {
    miLista.agregar(3);
    miLista.agregar(4);
 
-   System.out.println(" Lista despues de agregar elemntos: ");
-   milista.imprimir(); // Salida: 1 -> 2 -> 3 -> 4 -> null
+   System.out.println(" Lista despues de agregar elementos: ");
+   miLista.imprimir(); // Salida: 1 -> 2 -> 3 -> 4 -> null
 
-
+   //Eliminando un elemento
+   miLista.agregar(2);
+   System.out.println(" Lista despues de agregar 2: ");
+   miLista.imprimir(); // Salida: 1 -> 2 -> 3 -> 4 -> null
 
 
     }
---
+++ Sebas Calvera/ListaDoble.java
@@ -0,0 +1,30 @@
+public class ListaDoble {
+    NodoDoble cabeza;
+    NodoDoble cola;
+
+    public ListaDoble() {
+        this.cabeza = null;
+        this.cola = null;
+    }
+
+    // Método para insertar un nodo al inicio de la lista
+    public void insertarAlInicio(int valor) 
+
+    // Método para insertar un nodo al final de la lista
+    public void insertarAlFinal(int valor) 
+
+    // Método para eliminar un nodo
+    public void eliminar(int valor) 
+
+    // Método para buscar un nodo
+    public boolean buscar(int valor) 
+
+    // Mostrar de adelante hacia atrás
+    public void mostrarAdelante() 
+
+    // Mostrar de atrás hacia adelante
+    public void mostrarAtras() 
+
+    public static void main(String[] args) {
+     
+}
