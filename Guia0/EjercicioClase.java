584ef29ca0e7f819f2aafd38e612a7854723d081
SebasCalvera <112514206+SebasCalvera@users.noreply.github.com>
2025-09-22 17:37:51 -0500

    Add files via upload

---
+++ Guia0/EjercicioClase.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;        
+
+public class EjercicioClase {
+
+  
+   public static void main(String[] args) {
+    //Ejemplo de arreglo estatico
+    double[] calificaciones = new double [8]; ///Arreglo de 8 decimales
+    calificaciones [0] = 6.8;
+    calificaciones [1] = 8.3;
+    calificaciones [2] = 10;
+    calificaciones [3] = 9.5;
+    calificaciones [4] = 4.5;
+    calificaciones [5] = 7.7;
+    calificaciones [6] = 9.4;
+    calificaciones [7] = 10;
+   
+System.out.println(Arrays.toString(calificaciones));
+    
+
+   }
+    
+}
+
+
---
+++ Guia0/EjercicioClase2.java
@@ -0,0 +1,22 @@
+public class EjercicioClase2 {
+    public static void main(String[] args) {
+        
+   ListaEnlazada miLista = new ListaEnlazada();
+
+   //Agregando elementos a la lista
+   miLista.agregar(1);
+   miLista.agregar(2);
+   miLista.agregar(3);
+   miLista.agregar(4);
+
+   System.out.println(" Lista despues de agregar elementos: ");
+   miLista.imprimir(); // Salida: 1 -> 2 -> 3 -> 4 -> null
+
+   //Eliminando un elemento
+   miLista.agregar(2);
+   System.out.println(" Lista despues de agregar 2: ");
+   miLista.imprimir(); // Salida: 1 -> 2 -> 3 -> 4 -> null
+
+
+    }
+}
